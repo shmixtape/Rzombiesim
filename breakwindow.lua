aa81a5d09f9dcda9e47b4a5aa590bfac81c675cf
shmixtape <115726411+shmixtape@users.noreply.github.com>
2022-12-08 10:41:58 -0500

    Add files via upload

---
+++ breakwindow.lua
@@ -0,0 +1,12 @@
+
+local prompt = script.Parent.Base.ProximityPrompt
+
+
+prompt.Triggered:Connect(function()
+	prompt.Enabled = false	
+	for i,v in pairs(script.Parent.Break:GetChildren()) do
+		v.Anchored = false
+	end
+	wait(5)--How long till it destroys
+end)
+
